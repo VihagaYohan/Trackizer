3a868299f2227694f3ea390c64f91bc4dbfd2ac1
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-12-25 06:28:03 +0530

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by Yohan on 2024-12-25.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
